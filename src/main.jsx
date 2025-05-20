ebaae9027a8261de41f77ae2b3f5f715e9878adc
Olya Khromova <olya.hrmvlg@gmail.com>
2025-05-20 14:33:21 +0300

    Set up Redux store and create graph slice for state management

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import './assets/styles/index.scss';
+import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux';
+import { StrictMode } from 'react'
+import App from './App.jsx'
+import store from './store/index.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>,
 )
---
+++ src/slices/graphSlice.js
@@ -0,0 +1,13 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {};
+
+const graphSlice = createSlice({
+    name: 'graph',
+    initialState,
+    reducers: {
+        
+    },
+});
+
+export default graphSlice.reducer;
