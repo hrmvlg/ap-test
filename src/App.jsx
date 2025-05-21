308f4c23837cae00b9c082c89e59efeb80fa1d02
Olya Khromova <olya.hrmvlg@gmail.com>
2025-05-21 17:39:29 +0300

    refactor App component layout to include DownloadButton and improve UI structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Chart from './components/Chart/Chart';
 import CountryDropdown from './components/CountryDropdown/CountryDropdown';
 import styles from '../src/assets/styles/base.module.scss'
+import DownloadButton from './components/DownloadButton/DownloadButton';
 function App() {
 
   return (
     <div className={`container ${styles["wrapper-container"]}`}>
-      <CountryDropdown />
+      <div className="container d-flex justify-content-end" style={{ columnGap: '10px' }}>
+        <DownloadButton type="csv" />
+        <CountryDropdown />
+      </div>
       <Chart />
     </div>
   )
