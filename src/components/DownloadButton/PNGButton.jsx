528af7eb0d6d1e2bd49a5cfb206a9bca3e6e7906
Olya Khromova <olya.hrmvlg@gmail.com>
2025-05-21 18:37:27 +0300

    add PNGButton component for PNG export functionality

---
+++ src/components/DownloadButton/PNGButton.jsx
@@ -0,0 +1,12 @@
+import { Button } from "react-bootstrap";
+
+export default function PNGButton() {
+    return (
+        <Button
+            variant="outline-secondary"
+            style={{ color: "blue" }}
+        >
+            PNG
+        </Button>
+    )
+}
